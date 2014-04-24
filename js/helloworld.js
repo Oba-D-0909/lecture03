3425c3e6c81873de019fba1a75121add00804246
Oba-D-0909 <t13234do@sfc.keio.ac.jp>
2014-04-24 12:47:46 +0900

    あいうえお
    あいうえお

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,11 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+var teruteruteruya= function(){
+	var message = document.getElementById("teruteruteruya");
+	message.setAttribute("class", "");
+};
+
+var showteruteruteruyaButton = document.getElementById("obara");
+showteruteruteruyaButton.addEventListener("click", teruteruteruya);
